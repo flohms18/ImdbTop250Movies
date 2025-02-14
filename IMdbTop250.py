4568f8a9174d1b5dc27322b5fce8a20d43403f5e
flotr18 <trouilletflorent@outlook.com>
2025-02-14 15:10:37 +0100

    Rating by decade

--- IMdbTop250.py
+++ IMdbTop250.py
@@ -4,4 +4,16 @@ import matplotlib.pyplot as plt
 # Lire le fichier CSV
 df = pd.read_csv("IMDb_Top_250_Movies.csv", encoding="ISO-8859-1", dtype=object)
 
+df.info()
 
+df['Rating'] = pd.to_numeric(df['Rating'], errors ="coerce")
+df['Release_Year'] = pd.to_numeric(df['Release_Year'],errors="coerce")
+
+df = df.dropna()
+
+df['Decade'] = (df['Release_Year'] // 10 * 10)
+
+decade_rating = df.groupby("Decade")["Rating"].mean()
+
+decade_rating.plot(kind="bar",color="green", figsize=(10,6))
+plt.show()
