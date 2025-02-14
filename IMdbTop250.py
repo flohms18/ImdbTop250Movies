eb73f69fc175841e3e508ddfc12007ff7007f976
flotr18 <trouilletflorent@outlook.com>
2025-02-14 14:18:23 +0100

    Graph Made based on Duration and Release Year

--- IMdbTop250.py
+++ IMdbTop250.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("IMDb_Top_250_Movies.csv",encoding="ISO-8859-1", dtype=object)
-data = df.Duration
-print(data)
+
+df = pd.read_csv("IMDb_Top_250_Movies.csv", encoding="ISO-8859-1", dtype=object)
+data = df.head()
+
+data["Duration"] = pd.to_numeric(data["Duration"], errors="coerce")
+data["Release_Year"] = pd.to_numeric(data["Release_Year"], errors="coerce")
+data.plot(kind="bar",x="Release_Year",y="Duration",color="green")
+
+
+plt.show()
