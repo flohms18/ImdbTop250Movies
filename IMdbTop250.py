1a704d30913d2fd3fe7f6cc103b8127677c3dd1e
flotr18 <trouilletflorent@outlook.com>
2025-02-14 14:52:46 +0100

    Reset stuff

--- IMdbTop250.py
+++ IMdbTop250.py
@@ -1,15 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# Lire le fichier CSV
 df = pd.read_csv("IMDb_Top_250_Movies.csv", encoding="ISO-8859-1", dtype=object)
-data = df.head()
-
-df.info()
-
-data["Duration"] = pd.to_numeric(data["Duration"], errors="coerce")
-data["Name"] = data['Name'].astype(str)
-data.plot(kind="bar",y="Duration",x="Name",color="green")
 
 
-plt.show()
