ebfa055f74111f48a6a68a3e024912eed526ca74
flotr18 <trouilletflorent@outlook.com>
2025-02-14 15:12:30 +0100

    Votes instead of Rating

--- IMdbTop250.py
+++ IMdbTop250.py
@@ -6,14 +6,14 @@ df = pd.read_csv("IMDb_Top_250_Movies.csv", encoding="ISO-8859-1", dtype=object)
 
 df.info()
 
-df['Rating'] = pd.to_numeric(df['Rating'], errors ="coerce")
+df['Votes'] = pd.to_numeric(df['Votes'], errors ="coerce")
 df['Release_Year'] = pd.to_numeric(df['Release_Year'],errors="coerce")
 
 df = df.dropna()
 
 df['Decade'] = (df['Release_Year'] // 10 * 10)
 
-decade_rating = df.groupby("Decade")["Rating"].mean()
+decade_rating = df.groupby("Decade")["Votes"].mean()
 
 decade_rating.plot(kind="bar",color="green", figsize=(10,6))
 plt.show()
