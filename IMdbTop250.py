46518e78f610b6c09102729751f5334a88d37d3c
flotr18 <trouilletflorent@outlook.com>
2025-02-14 11:42:57 +0100

    Read CSV file with csv module

--- IMdbTop250.py
+++ IMdbTop250.py
@@ -1,4 +1,5 @@
-def hello():
-    print("Hello")
-
-hello()
+import csv 
+with open('IMDB_Top_250_Movies.csv',newline='', encoding="latin1") as csvfile:
+    MovieReader = csv.reader(csvfile, delimiter=' ',quotechar='|')
+    for row in MovieReader:
+        print(', '.join(row))
